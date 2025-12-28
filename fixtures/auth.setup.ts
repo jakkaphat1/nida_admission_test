16ceda81c5643b1024630a4f5bac737ede804d42
jakkaphat1 <jakkaphat.w@kkumail.com>
2025-12-28 12:33:21 +0700

    [Edit & Create]--> Edit auth.setup.ts and create Auth.txt for step to bypass authen. for many test case

--- fixtures/auth.setup.ts
+++ fixtures/auth.setup.ts
@@ -1,14 +1,18 @@
+// fixtures/auth.setup.ts
 import { test as setup } from '@playwright/test';
 
 const authFile = 'playwright/.auth/user.json';
 
 setup('authenticate', async ({ page }) => {
+  // 1. ไปที่หน้าแรกของระบบ
   await page.goto('https://admissions-uat.nida.ac.th/');
-  await page.getByRole('button', { name: 'Login with Google' }).click();
 
-  await page.getByText('jakkaphat.w@kkumail.com').click();
+  // 2. คลิกปุ่ม เข้าใช้งานระบบ
+  await page.getByRole('button', { name: 'เข้าใช้งานระบบ' }).click();
 
-  // รอจนกลับมาหน้าหลัก และบันทึก Session
-  await page.waitForURL(/.*admissions-uat.nida.ac.th/);
+  // 3. กรอกอีเมล manual   ก่อน
+  await page.pause();
+
+  // 4. บันทึก Session (Cookies & Local Storage) ลงในไฟล์ user.json
   await page.context().storageState({ path: authFile });
 });
