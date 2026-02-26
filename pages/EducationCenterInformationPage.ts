ab6a82b381623e3a19e3159ce42609272d49127e
jakkaphat1 <jakkaphat.w@kkumail.com>
2026-02-26 22:01:11 +0700

    [Add]->TC-04 ทดสอบเพิ่มข้อมูลศูนย์การศึกษา

--- pages/EducationCenterInformationPage.ts
+++ pages/EducationCenterInformationPage.ts
@@ -85,4 +85,153 @@ export class EducationCenterInformationPage{
         }
     }
 
+    async clickAddEducationCentralButton(){
+        const addButton = this.page.getByRole('button', { name: 'เพิ่มข้อมูลศูนย์การศึกษา' })
+        await addButton.click()
+    }
+
+    async fillAddCentralEduInfoPage(data:{
+        centralEduID? : string,
+        centralEduNameTH? : string,
+        centralEduNameEN? : string,
+        centralEduNameCN? : string,
+        facultyName? : string
+        
+    }){
+        if(data.centralEduID){
+            const centralEduIDBox = this.page.getByRole('textbox', { name: 'รหัสศูนย์การศึกษา*' })
+            await centralEduIDBox.fill(data.centralEduID)
+        }
+
+        if(data.centralEduNameTH){
+            const centralEduNameTHBox = this.page.getByRole('textbox', { name: 'ชื่อศูนย์การศึกษา (ภาษาไทย)*' })
+            await centralEduNameTHBox.fill(data.centralEduNameTH)
+        }
+
+        if(data.centralEduNameEN){
+            const centralEduNameENBox = this.page.getByRole('textbox', { name: 'ชื่อศูนย์การศึกษา (ภาษาอังกฤษ)' })
+            await centralEduNameENBox.fill(data.centralEduNameEN)
+        }
+
+        if(data.centralEduNameCN){
+            const centralEduNameCNBox = this.page.getByRole('textbox', { name: 'ชื่อศูนย์การศึกษา (ภาษาจีน)' })
+            await centralEduNameCNBox.fill(data.centralEduNameCN)
+        }
+
+        if(data.facultyName){
+            const facultyDropdown = this.page.locator('.react-select__input-container').first()
+            const facultyOption = this.page.getByRole('option' , { name : data.facultyName})
+            await facultyDropdown.click()
+            await facultyOption.click()
+            await facultyDropdown.click()
+        }
+    }
+
+    async fillAddFacultyAddressInfo(data:{
+        centralEduAddress? : string,
+        centralEduProvince? : string,
+        centralEduDistrict? : string,
+        centralEduSubDistrict? : string,
+        centralEduZipCode? : string,
+        centralEduLatitude? : string,
+        centralEduLongitude? : string,
+    }){
+        if(data.centralEduAddress){
+            const address = this.page.getByRole('textbox', { name: 'ที่อยู่' })
+            await address.fill(data.centralEduAddress)
+        }
+        if(data.centralEduProvince){
+            const provinceDropdown = this.page.locator('#province_code > .unext-form-control > .react-select__indicators')
+            const provinceOption = this.page.getByRole('option', { name: data.centralEduProvince })
+            await provinceDropdown.click()
+            await provinceOption.click()
+        }
+        if(data.centralEduDistrict){
+            const districtDropdown = this.page.locator('#district_code > .unext-form-control > .react-select__value-container > .react-select__input-container')
+            const districtOption = this.page.getByRole('option', { name: data.centralEduDistrict })
+            await districtDropdown.click()
+            await districtOption.click()
+        }
+        if(data.centralEduSubDistrict){
+            const centralEduSubDistrictDropdown = this.page.locator('#subdistrict_code > .unext-form-control > .react-select__indicators')
+            const camputSubDistrictOption = this.page.getByRole('option', { name: data.centralEduSubDistrict })
+            await centralEduSubDistrictDropdown.click()
+            await camputSubDistrictOption.click()
+        }
+        if(data.centralEduZipCode){
+            const zipcodeDropdown = this.page.locator('#zipcode > .unext-form-control > .react-select__indicators')
+            const zipcodeOption = this.page.getByRole('option', { name: data.centralEduZipCode })
+            await zipcodeDropdown.click()
+            await zipcodeOption.click()
+        }
+        if(data.centralEduLatitude){
+            const latitudeBox = this.page.getByRole('textbox', { name: 'Latitude' })
+            await latitudeBox.fill(data.centralEduLatitude)
+        }
+        if(data.centralEduLongitude){
+            const longitudeBox = this.page.getByRole('textbox', { name: 'Longitude' })
+            await longitudeBox.fill(data.centralEduLongitude)
+        }
+    }
+
+    async fillAddFacultyContactInfo(data:{
+        centralEduTel? : string,
+        centralEduLineID? : string,
+        centralEduFacebook? : string,
+        centralEduEmail? : string,
+        centralEduWebsite? : string,
+        centralEduFirstOfficerName? : string,
+        centralEduFirstOfficerTel? : string,
+        centralEduSecondOfficerName? : string,
+        centralEduSecondOfficerTel? : string,
+    }){
+        if(data.centralEduTel){
+            const telphoneBox = this.page.getByRole('textbox', { name: 'เบอร์โทรสำนักงาน' })
+            await telphoneBox.fill(data.centralEduTel)
+        }
+        if(data.centralEduLineID){
+            const lineIDBox = this.page.getByRole('textbox', { name: 'line ID' })
+            await lineIDBox.fill(data.centralEduLineID)
+        }
+        if(data.centralEduFacebook){
+            const faceboolBox = this.page.getByRole('textbox', { name: 'Facebook' })
+            await faceboolBox.fill(data.centralEduFacebook)
+        }
+        if(data.centralEduEmail){
+            const inputBox = this.page.getByRole('textbox', { name: 'Email' })
+            await inputBox.fill(data.centralEduEmail)
+        }
+        if(data.centralEduWebsite){
+            const inputBox = this.page.getByRole('textbox', { name: 'Website' })
+            await inputBox.fill(data.centralEduWebsite)
+        }
+        if(data.centralEduFirstOfficerName && data.centralEduFirstOfficerTel ){
+            const inputNameBox = this.page.locator('[id="officers[0].contact_name"]')
+            const inputTelBox = this.page.locator('[id="officers[0].contact_tel"]')
+            await inputNameBox.fill(data.centralEduFirstOfficerName)
+            await inputTelBox.fill(data.centralEduFirstOfficerTel)
+        }
+        if(data.centralEduSecondOfficerName && data.centralEduSecondOfficerTel){
+            const inputNameBox = this.page.locator('[id="officers[1].contact_name"]')
+            const inputTelBox = this.page.locator('[id="officers[1].contact_tel"]')
+            await inputNameBox.fill(data.centralEduSecondOfficerName)
+            await inputTelBox.fill(data.centralEduSecondOfficerTel)
+        }
+    }
+
+    async handleStatusSlider(status:string){
+        const statusContainer = this.page.locator('span.label')
+        if (status) {
+            const currentText = await statusContainer.textContent()
+            if (!currentText?.includes(status)) {
+                await statusContainer.click()
+            }
+            await expect(statusContainer).toContainText(status)
+        }
+    }
+
+    async clickSaveButton(){
+        const saveButton = this.page.getByRole('button', { name: 'บันทึก' })
+        await saveButton.click()
+    }
 }
--- tests/Officer/nida_education_center_information.spec.ts
+++ tests/Officer/nida_education_center_information.spec.ts
@@ -29,5 +29,47 @@ test.describe('Test Script - NIDA Backoffice โมดูล ADM งานรั
         await expect(page.getByText('ศูนย์การศึกษาจังหวัดกรุงเทพมหานคร - Bangkok Education Center')).toBeVisible()
     });
     
+    test('TC-04 ทดสอบเพิ่มข้อมูลศูนย์การศึกษา' , async ({ educationCenterInformationPage, page}) => {
+        await educationCenterInformationPage.gotoPrograms();
+        await educationCenterInformationPage.gotoEducationCenterMenu()
+        await expect(page).toHaveURL(/.*admin\/admission\/master\/education-center/);
+        await educationCenterInformationPage.clickAddEducationCentralButton()
+        await expect(page).toHaveURL(/.*admin\/admission\/master\/education-center\/create.*/);
 
+        const centralEduBasicInfo = {
+            centralEduID : 'PKT001',
+            centralEduNameTH : 'ศูนย์การศึกษาจังหวัดภูเก็ต',
+            centralEduNameEN : 'Phuket Educationi Center',
+            centralEduNameCN : '',
+            facultyName : 'GSEDA'
+        }
+        await educationCenterInformationPage.fillAddCentralEduInfoPage(centralEduBasicInfo)
+
+        const centralEduAddessInfo = {
+            centralEduAddress : 'วิทยาลัยอาชีวศึกษาภูเก็ต อาคารรัษฎา',
+            centralEduProvince : 'ภูเก็ต',
+            centralEduDistrict : 'เมืองภูเก็ต',
+            centralEduSubDistrict : 'ตลาดใหญ่',
+            centralEduZipCode: '83000',
+            centralEduLatitude : '7.8949373638514',
+            centralEduLongitude : '98.3768345189004',
+        }
+        await educationCenterInformationPage.fillAddFacultyAddressInfo(centralEduAddessInfo)
+
+        const centralEduContactInfo = {
+            centralEduTel : '02-123-24770',
+            centralEduLineID : '',
+            centralEduFacebook : 'ท่องเที่ยว นิด้า GSTM NIDA - PKT',
+            centralEduEmail : 'gstm@nida.ac.th',
+            centralEduWebsite : 'https://tour.nida.ac.th/th/',
+            centralEduFirstOfficerName : 'นางสาวสิรินภา จงเกษกรณ์',
+            centralEduFirstOfficerTel : '09-485-98905',
+            centralEduSecondOfficerName : '',
+            centralEduSecondOfficerTel : '',
+        }
+        await educationCenterInformationPage.fillAddFacultyContactInfo(centralEduContactInfo)
+        await educationCenterInformationPage.handleStatusSlider('ใช้งาน')
+        await educationCenterInformationPage.clickSaveButton()
+        await expect(page.getByText('บันทึกข้อมูลสำเร็จ')).toBeVisible()
+    });
 });
