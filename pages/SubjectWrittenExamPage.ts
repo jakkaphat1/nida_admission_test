9026e5eb91bfae2bb8f1433cf891bf8183ea6bf4
jakkaphat1 <jakkaphat.w@kkumail.com>
2026-03-13 15:32:01 +0700

    [Add]-> TC-04 ทดสอบกำหนดข้อมูลวิชาที่เปิดสอบข้อเขียน

--- pages/SubjectWrittenExamPage.ts
+++ pages/SubjectWrittenExamPage.ts
@@ -1,5 +1,19 @@
 import { Page, Locator , expect } from '@playwright/test'; 
+import { count } from 'node:console';
+type DateString = `${string}`;
 
+type CalendarField =
+  | "วันที่เปิดรับสมัครสอบข้อเขียน"
+  | "วันประกาศรายชื่อผู้มีสิทธิ์สอบและสถานที่สอบ"
+  | "วันที่สอบ"
+  | "วันประกาศผลสอบข้อเขียน";
+  
+
+interface CalendarDateInput {
+  field: CalendarField;
+  startDate: DateString;
+  endDate: DateString;
+}
 export class SubjectWrittenExamPage {
     page : Page;
     /**
@@ -99,5 +113,145 @@ export class SubjectWrittenExamPage {
             await statusTypeOption.click()
         }
         await backfilterBtn.click()
+    }
+
+    async addSubjectWrittenExamButton(){
+        const writtenExamBtn = this.page.getByRole('button', { name: 'เพิ่มข้อมูล' })
+        await writtenExamBtn.click()
+    }
+
+    //กรอกข้อมูลหน้าเพิ่มวิชาที่เปิดสอบข้อเขียน
+    async fillAddWrittenExamPageStep1(data:{
+        eduYear?:string
+        semester?:string
+        round?:string
+        eduLevel?:string
+        studentType?:string
+    }){
+        if(data.eduYear){
+            const eduYearDropdown = this.page.locator('.react-select__indicators').first()
+            const eduYearOption = this.page.getByRole('option', { name: data.eduYear })
+            await eduYearDropdown.click()
+            await eduYearOption.click()
+        }
+
+        if(data.semester){
+            const semesterDropdown = this.page.locator('#semester > .unext-form-control > .react-select__indicators')
+            const semesterOption = this.page.getByRole('option', { name: data.semester })
+            await semesterDropdown.click()
+            await semesterOption.click()
+        }
+
+        if(data.round){
+            const roundDropdown = this.page.locator('#round > .unext-form-control > .react-select__indicators')
+            const roundOption = this.page.getByRole('option', { name: data.round , exact:true } )
+            await roundDropdown.click()
+            await roundOption.click()
+        }
+
+        if(data.eduLevel){
+            const eduLevelDropdown = this.page.locator('#edulevel_code > .unext-form-control > .react-select__indicators')
+            const eduLevelOption = this.page.getByRole('option', { name: data.eduLevel })
+            await eduLevelDropdown.click()
+            await eduLevelOption.click()
+        }
+        
+        if(data.studentType){
+            const studentTypeDropdown = this.page.locator('#student_status_code > .unext-form-control > .react-select__indicators')
+            const studentTypeOption = this.page.getByRole('option', { name: data.studentType })
+            await studentTypeDropdown.click()
+            await studentTypeOption.click()
+        }
+    }
+
+    async fillAddWrittenExamPageStep2(data:{
+        subject?:string
+        changeDate?:'Yes' | 'No'
+        startDate?:string
+        endDate?:string
+        fee?:string
+    }){
+        const viewForSubject = this.page.locator('.card-container').filter({ hasText: data.subject }).first()
+        const selectSubjectCheckBox = viewForSubject.locator('#exam_id')
+        
+        const startDateBox = this.page.getByRole('textbox', { name: 'DD/MM/YYYY' }).first()
+        const endDateBox = this.page.getByRole('textbox', { name: 'DD/MM/YYYY' }).nth(1)
+        if(data.subject){
+            await selectSubjectCheckBox.click()
+        }
+        if(data.changeDate=='Yes'){
+            const keywordBox = this.page.locator('div').filter({ hasText: /^เปลี่ยนแปลงวันที่เปิดรับ\*$/ }).nth(2)
+            const selectChangeDate = keywordBox.getByRole('checkbox').first()
+            await selectChangeDate.click()
+        }
+        if(data.startDate && data.endDate){
+            await startDateBox.click({clickCount:3})
+            await startDateBox.pressSequentially(data.startDate,{delay:100})
+            await endDateBox.click({clickCount:3})
+            await endDateBox.pressSequentially(data.endDate,{delay:100})
+        }
+        if (data.fee) {
+            const feeRadio = viewForSubject.locator('input[type="radio"]').filter({ 
+                has: this.page.locator('..').filter({ hasText: data.fee }) 
+            }).first();
+            const feeOption = viewForSubject.locator('div').filter({ hasText: data.fee }).last();
+            await feeOption.click();
+            await feeRadio.check({ force: true });
+        }
+    }
+    async handleStatusToggle(targetStatus: 'ใช้งาน' | 'ไม่ใช้งาน'){
+        const statusToggle = this.page.locator('span.label');
+        const currentStatus = await statusToggle.innerText();
+        console.log(`Current: ${currentStatus.trim()} -> Target: ${targetStatus}`);
+
+        if (currentStatus.trim() !== targetStatus){
+            await statusToggle.click()
+            await expect(this.page.getByText(targetStatus , {exact:true})).toBeVisible()
+        }
+    }
+
+    async fillScheduleDates(data: CalendarDateInput[]) {
+        for (const item of data) {
+            const escapedField = item.field.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+            const row = this.page.locator("div").filter({ 
+                hasText: new RegExp(`^\\d+${escapedField}$`) 
+            }).first();
+            const startDateInput = row.getByRole("textbox", { name: "DD/MM/YYYY" }).first();
+            const endDateInput   = row.getByRole("textbox", { name: "DD/MM/YYYY" }).nth(1);
+
+            console.log(`กำลังกรอก field: "${item.field}" | start: ${item.startDate} → end: ${item.endDate}`)
+
+            await startDateInput.click();
+            await startDateInput.pressSequentially(item.startDate );
+            await this.page.keyboard.press("Tab");
+
+            await endDateInput.click();
+            await endDateInput.pressSequentially(item.endDate );
+            await this.page.keyboard.press("Tab");
+            await row.click()
+            console.log(`กรอกเสร็จ: "${item.field}"`)
+        }
+    }
+
+    async clickSaveButton(){
+        const saveBtn = this.page.getByRole('button', { name: 'บันทึก' })
+        await saveBtn.click()
+    }
+
+    async confirmSaveButton(){
+        const confirmBtn = this.page.getByRole('button', { name: 'ยืนยัน' })
+        await expect(this.page.getByRole('heading', { name: 'ยืนยันการบันทึก' })).toBeVisible()
+        await confirmBtn.click()
+    }
+
+    async clickNextButton(){
+        const nextBtn = this.page.getByRole('button', { name: 'ถัดไป' })
+        await nextBtn.click()
+    }
+
+    async clickConfirmSave(){
+        const confirmBtn = this.page.getByRole('button', { name: 'ยืนยัน' })
+        await expect(this.page.getByRole('heading', { name: 'ยืนยันการบันทึก' })).toBeVisible() 
+        await confirmBtn.click()
     }
 }
--- tests/Officer/nida_subject_written_exam.spec.ts
+++ tests/Officer/nida_subject_written_exam.spec.ts
@@ -30,4 +30,61 @@ test.describe('Test Script - NIDA Backoffice โมดูล ADM งานรั
         })
         await expect(page.getByText('รอบที่ 1/2570 (ภาคการศึกษาที่ 1) - ระยะเวลารับสมัครเรียน (13/03/2569 - 13/03/2569)')).toBeVisible()
     });
+
+    test('TC-04 ทดสอบกำหนดข้อมูลวิชาที่เปิดสอบข้อเขียน' , async ({ commonPage , subjectWrittenExam , page}) => {
+        test.setTimeout(40000)
+        await commonPage.gotoPrograms()
+        await subjectWrittenExam.gotoSubjectWrittenExamMenu()
+        await expect(page).toHaveURL(/.*admin\/admission\/transaction\/subject/);
+        await subjectWrittenExam.checkSubjectWrittenExamMenu()
+        await subjectWrittenExam.addSubjectWrittenExamButton()
+        await expect(page).toHaveURL(/.*admin\/admission\/transaction\/subject\/create.*/);
+        await subjectWrittenExam.fillAddWrittenExamPageStep1({
+            eduYear:'2570',
+            semester:'ภาคการศึกษาที่ 1',
+            round:'2',
+            eduLevel:'ปริญญาโท',
+            studentType:'ภาคปกติ'
+        })
+        await subjectWrittenExam.handleStatusToggle('ใช้งาน')
+        await subjectWrittenExam.fillScheduleDates([
+            {
+                field: "วันที่เปิดรับสมัครสอบข้อเขียน",
+                startDate: "13032569",
+                endDate: "13032569",
+            },
+            {
+                field: "วันประกาศรายชื่อผู้มีสิทธิ์สอบและสถานที่สอบ",
+                startDate: "13032569",
+                endDate: "13032569",
+            },
+            {
+                field: "วันที่สอบ",
+                startDate: "13032569",
+                endDate: "13032569",
+            },
+            {
+                field: "วันประกาศผลสอบข้อเขียน",
+                startDate: "13032569",
+                endDate: "13032569",
+            }
+            
+        ]);
+        await subjectWrittenExam.clickSaveButton()
+        await subjectWrittenExam.confirmSaveButton()
+        await expect(page.getByText('บันทึกข้อมูลสำเร็จ')).toBeVisible()
+        await subjectWrittenExam.clickNextButton()
+
+        await subjectWrittenExam.fillAddWrittenExamPageStep2({
+            subject:'วิชาเฉพาะ 2' ,
+            changeDate:'Yes' as const,
+            startDate:'13032569' ,
+            endDate:'14032569' ,
+            fee:'FF00000045'
+        })
+        await subjectWrittenExam.clickSaveButton()
+        await subjectWrittenExam.clickConfirmSave()
+        await expect(page.getByText('บันทึกข้อมูลสำเร็จ')).toBeVisible()
+        await expect(page).toHaveURL(/.*admin\/admission\/transaction\/subject/);
+    });
 });
