798e8377944846a505e05be21ba5403d6c1a16d5
jakkaphat1 <jakkaphat.w@kkumail.com>
2026-03-13 13:59:04 +0700

    [Add]-> TC-03 ทดสอบค้นหาข้อมูลวิชาที่เปิดสอบข้อเขียนที่ยังไม่ประกาศรับสมัคร

--- pages/SubjectWrittenExamPage.ts
+++ pages/SubjectWrittenExamPage.ts
@@ -32,4 +32,72 @@ export class SubjectWrittenExamPage {
         await expect(notOpen).toBeVisible()
         await expect(opened).toBeVisible()
     }
+
+    async fillSearchBox(searchKeyword:string){
+        const searchBox = this.page.getByRole('textbox', { name: 'ค้นหาจากรหัส หรือชื่อวิชา' })
+        await searchBox.pressSequentially(searchKeyword)
+    }
+
+    async clickStatusByKeyword(status:string){
+        const statusDropdown = this.page.locator('.react-select__value-container').first()
+        const statusOption = this.page.getByRole('option', { name: status, exact: true })
+        await statusDropdown.click()
+        await statusOption.click()
+    }
+
+    async filterMoreOption(data:{
+        eduYear?:string
+        semester?:string
+        round?:string
+        eduLevel?:string
+        studentType?:string
+        status?:string
+    }){
+        const filterBtn = this.page.getByRole('button', { name: 'ตัวกรอง' })
+        const backfilterBtn = this.page.getByRole('button', { name: 'ตัวกรอง' }).nth(1)
+        await filterBtn.click()
+
+        if(data.eduYear){
+            const eduYearDropdown = this.page.locator('div').filter({ hasText: /^ปีการศึกษา$/ }).nth(3)
+            const eduYearOption = this.page.getByRole('option', { name: data.eduYear })
+            await eduYearDropdown.click()
+            await eduYearOption.click()
+        }
+
+        if(data.semester){
+            const semesterDropdown = this.page.locator('div').filter({ hasText: /^ภาคการศึกษา$/ }).nth(3)
+            const semesterOption = this.page.getByRole('option', { name: data.semester })
+            await semesterDropdown.click()
+            await semesterOption.click()
+        }
+
+        if(data.round){
+            const roundDropdown = this.page.locator('div').filter({ hasText: /^ครั้งที่$/ }).nth(3)
+            const roundOption = this.page.getByRole('option', { name: data.round , exact:true } )
+            await roundDropdown.click()
+            await roundOption.click()
+        }
+
+        if(data.eduLevel){
+            const eduLevelDropdown = this.page.locator('div').filter({ hasText: /^ระดับการศึกษา$/ }).nth(3)
+            const eduLevelOption = this.page.getByRole('option', { name: data.eduLevel })
+            await eduLevelDropdown.click()
+            await eduLevelOption.click()
+        }
+        
+        if(data.studentType){
+            const studentTypeDropdown = this.page.locator('div').filter({ hasText: /^ประเภทนักศึกษา$/ }).nth(3)
+            const studentTypeOption = this.page.getByRole('option', { name: data.studentType })
+            await studentTypeDropdown.click()
+            await studentTypeOption.click()
+        }
+
+        if(data.status){
+            const statusTypeDropdown = this.page.locator('div').filter({ hasText: /^เลือกสถานะทั้งหมด$/ }).nth(3)
+            const statusTypeOption = this.page.getByRole('option', { name: data.status ,exact:true }).first()
+            await statusTypeDropdown.click()
+            await statusTypeOption.click()
+        }
+        await backfilterBtn.click()
+    }
 }
--- tests/Officer/nida_subject_written_exam.spec.ts
+++ tests/Officer/nida_subject_written_exam.spec.ts
@@ -13,4 +13,21 @@ test.describe('Test Script - NIDA Backoffice โมดูล ADM งานรั
         await expect(page).toHaveURL(/.*admin\/admission\/transaction\/subject/);
         await subjectWrittenExam.checkSubjectWrittenExamMenu()
     });
+
+    test('TC-03 ทดสอบค้นหาข้อมูลวิชาที่เปิดสอบข้อเขียนที่ยังไม่ประกาศรับสมัคร' , async ({ commonPage , subjectWrittenExam , page}) => {
+        await commonPage.gotoPrograms()
+        await subjectWrittenExam.gotoSubjectWrittenExamMenu()
+        await expect(page).toHaveURL(/.*admin\/admission\/transaction\/subject/);
+        await subjectWrittenExam.checkSubjectWrittenExamMenu()
+        await subjectWrittenExam.fillSearchBox('วิชาเฉพาะ 2')
+        await subjectWrittenExam.filterMoreOption({
+            eduYear:'2570',
+            semester:'ภาคการศึกษาที่ 1',
+            round:'1',
+            eduLevel:'ปริญญาโท',
+            studentType:'ภาคปกติ',
+            status:'ใช้งาน'
+        })
+        await expect(page.getByText('รอบที่ 1/2570 (ภาคการศึกษาที่ 1) - ระยะเวลารับสมัครเรียน (13/03/2569 - 13/03/2569)')).toBeVisible()
+    });
 });
