0e9ea1546273d793699b04a4c0bb1f75e0d7c2a3
jakkaphat1 <jakkaphat.w@kkumail.com>
2026-03-17 10:00:46 +0700

    [Add]-> TC-02 เมนูข้อมูลรายชื่อผู้มีสิทธิ์สอบประจำวิชาเฉพาะ

--- pages/EligibleWrittenExamPage.ts
+++ pages/EligibleWrittenExamPage.ts
@@ -6,16 +6,32 @@ export class EligibleWrittenExamPage {
     /**
  * LOCATORS SECTION
  * ---------------------------------------------------------------- */
-
+    notAnnouceTab
+    annoucedTab
     /**
  * Constructor SECTION
  * ---------------------------------------------------------------- */
     constructor(page:Page) {
         this.page = page;
-
+        this.notAnnouceTab = this.page.getByRole('button', { name: 'ยังไม่ประกาศรายชื่อผู้มีสิทธิ์สอบ' })
+        this.annoucedTab = this.page.getByRole('button', { name: 'ประกาศรายชื่อผู้มีสิทธิ์สอบแล้ว' })
     }
     /**
  * Method SECTION
  * ---------------------------------------------------------------- */    
-    
+    async gotoEligibleWrittenExamListMenu(){
+        const applicationWork = this.page.getByRole('listitem', { name: 'งานรับสมัคร' })
+        const basicInfoButton = this.page.getByRole('listitem', { name: 'การทำงาน' }).nth(3)
+        const writtenApplicationListItem = this.page.getByRole('listitem', { name: 'การสมัครสอบวิชาเฉพาะ' })
+        const eligibleWrittenExamMenu = this.page.getByRole('link', { name: 'ข้อมูลรายชื่อผู้มีสิทธิ์สอบประจำวิชาเฉพาะ' })
+        await applicationWork.click()
+        await basicInfoButton.click()
+        await writtenApplicationListItem.click()
+        await eligibleWrittenExamMenu.click()
+    }
+
+    async checkEligibleWrittenExamMenu(){
+        await expect(this.notAnnouceTab).toBeVisible();
+        await expect(this.annoucedTab).toBeVisible()
+    }
 }
--- tests/Officer/nida_written_eligible_list.spec.ts
+++ tests/Officer/nida_written_eligible_list.spec.ts
@@ -7,5 +7,10 @@ test.describe('Test Script - NIDA Backoffice โมดูล ADM งานรั
         await commonPage.fillUsernameAndPassword('sys_admin1','unext@2022')
     });
 
-    
+    test('TC-02 เมนูข้อมูลรายชื่อผู้มีสิทธิ์สอบประจำวิชาเฉพาะ' , async ({ commonPage , eligibleWrittenExamPage , page}) => {
+        await commonPage.gotoPrograms()
+        await eligibleWrittenExamPage.gotoEligibleWrittenExamListMenu()
+        await expect(page).toHaveURL(/.*admin\/admission\/transaction\/eligible-for-quiz-list.*/);
+        await eligibleWrittenExamPage.checkEligibleWrittenExamMenu()
+    });
 });
