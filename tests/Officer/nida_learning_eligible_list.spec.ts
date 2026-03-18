f1560e0bbb2e66ff8cd64ec1f3a6bef4f3db144c
jakkaphat1 <jakkaphat.w@kkumail.com>
2026-03-18 22:31:34 +0700

    [Add]-> TC-09 ทดสอบยกเลิกประกาศ

--- tests/Officer/nida_learning_eligible_list.spec.ts
+++ tests/Officer/nida_learning_eligible_list.spec.ts
@@ -216,4 +216,27 @@ test.describe('Test Script - NIDA Backoffice โมดูล ADM งานรั
         await eligibleLearningPage.clickSeePDFinHistory()
         await eligibleLearningPage.clickClosePDFHistoryPopup()
     });
+
+    test('TC-09 ทดสอบยกเลิกประกาศ' , async ({ commonPage , eligibleLearningPage , page}) => {
+        test.setTimeout(45000)
+        await commonPage.gotoPrograms()
+        await eligibleLearningPage.gotoEligibleLearningMenu()
+        await expect(page).toHaveURL(/.*admin\/admission\/transaction\/eligible-candidates-list-for-program/);
+        await eligibleLearningPage.checkLearningEligibleMenu()
+        await eligibleLearningPage.clickAnyTabByKeyword('ประกาศรายชื่อผู้มีสิทธิ์สอบประจำโครงการ')
+        await eligibleLearningPage.clickStatusByKeyword('ใช้งาน')
+        await eligibleLearningPage.filterMoreOption({
+            eduYear:'2569',
+            // semester:'ภาคการศึกษาที่ 1',
+            // eduLevel:'ปริญญาโท',
+            // studentType:'ภาคปกติ',
+            round:'7',
+        })
+        await eligibleLearningPage.clickExpandDetailButtonByName('รอบที่ 7/2569 (ภาคการศึกษาที่ 1)')
+        await eligibleLearningPage.clickSeeAnnoucementButtonByCard('รอบที่ 7/2569 (ภาคการศึกษาที่ 1)')
+        await expect(page).toHaveURL(/.*admin\/admission\/transaction\/eligible-candidates-list-for-program\/view-announcement.*/);
+        await eligibleLearningPage.clickKebabButtonForCancelAnnouceByNumber('18032026173922TDn')
+        await eligibleLearningPage.clickConfirmCancelAnnoucePopup()
+        await expect(page.getByRole('alert').filter({ hasText: 'บันทึกรายการเรียบร้อยแล้ว' }).last()).toBeVisible()
+    });
 });
