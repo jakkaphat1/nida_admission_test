9bc0a4793023a88169255157a952344191e1693b
jakkaphat1 <jakkaphat.w@kkumail.com>
2026-03-18 17:16:09 +0700

    [Add]-> TC-04 ทดสอบนำออกรายชื่อผู้มีสิทธิ์สอบประจำโครงการ

--- pages/Student/EligibleLearningPage.ts
+++ pages/Student/EligibleLearningPage.ts
@@ -35,6 +35,18 @@ export class EligibleLearningPage {
         await expect(annouced).toBeVisible()
     }
 
+    async clickAnyTabByKeyword(tab:'ยังไม่ประกาศรายชื่อผู้มีสิทธิ์สอบประจำโครงการ'|'ประกาศรายชื่อผู้มีสิทธิ์สอบประจำโครงการ'){
+        const tabBtn = this.page.getByRole('button', { name: tab })
+        const tabBtnExact = this.page.getByRole('button', { name: tab}).nth(1)
+        if(tab==='ยังไม่ประกาศรายชื่อผู้มีสิทธิ์สอบประจำโครงการ'){
+            await tabBtn.click()
+        }
+        if(tab==='ประกาศรายชื่อผู้มีสิทธิ์สอบประจำโครงการ'){
+            await tabBtnExact.click()
+        }
+        
+    }
+
     //method ค้นหาในกล่องค้นหา
     async fillSearchBox(searchKeyword:string){
         const searchBox = this.page.getByRole('textbox', { name: 'ค้นหาจากรหัส หรือชื่อหลักสูตรและโครงการ' })
@@ -98,4 +110,87 @@ export class EligibleLearningPage {
         await statusOption.click()
     }
 
+//Verify
+    async clickVerifyDetailButtonByCard(cardName:string){
+        const card = this.page.locator('div').filter({ hasText: cardName }).nth(5)
+        const verifyDetailBtn = card.getByRole('button', { name: 'ตรวจสอบข้อมูล' })
+        await verifyDetailBtn.click()
+    }
+
+    async checkEligibleStudentDetailPage(...texts: string[]){
+        const eligibleStudentTextBox = this.page.getByRole('textbox', { name: 'ค้นหาจากรหัส หรือชื่อ-นามสกุลใบสมัคร' })
+        await expect(eligibleStudentTextBox).toBeVisible()
+
+        for (const text of texts) {
+            const element = this.page.getByText(text)
+            await element.highlight()
+            await expect(element).toBeVisible()
+        }
+    }
+
+    async fillSearchEligibleLearningStudent(studentName:string){
+        const texBox = this.page.getByRole('textbox', { name: 'ค้นหาจากรหัส หรือชื่อ-นามสกุลใบสมัคร' })
+        await texBox.pressSequentially(studentName)
+    }
+
+    async filterEligibleSearchOption(data: {
+        status?: string
+        faculty?: string
+        course?: string
+        program?: string
+    }) {
+        const statusDropdown = this.page.locator('div').filter({ hasText: /^สถานะ$/ }).nth(3)
+        const statusOption = this.page.getByRole('option', { name: data.status })
+        const facutltyDropdown = this.page.locator('div').filter({ hasText: /^คณะ$/ }).nth(3)
+        const facutltyOption = this.page.getByRole('option', { name: data.faculty })
+        const courseDropdown = this.page.locator('div').filter({ hasText: /^หลักสูตร$/ }).nth(3)
+        const courseOption = this.page.getByRole('option', { name: data.course })
+        const programDropdown = this.page.locator('div').filter({ hasText: /^โครงการ$/ }).nth(3)
+        const programOption = this.page.getByRole('option', { name: data.program })
+
+        const filterBtn = this.page.getByRole('button', { name: 'ตัวกรอง' })
+        const filterBackBtn = this.page.getByRole('button', { name: 'ตัวกรอง' }).nth(1)
+        
+        await filterBtn.click()
+        if (data.status) {
+            await statusDropdown.click()
+            await statusOption.click()
+        }
+        if (data.faculty) {
+            await facutltyDropdown.click()
+            await facutltyOption.click()
+        }
+        if (data.course) {
+            await courseDropdown.click()
+            await courseOption.click()
+        }
+        if (data.program) {
+            await programDropdown.click()
+            await programOption.click()
+        }
+        await filterBackBtn.click()
+    }
+
+    async clickExpandDetailButtonByCourse(cardName:string){
+        const card = this.page.locator('div').filter({ hasText: cardName }).nth(5)
+        const expandDetailBtn = card.getByRole('button').filter({ hasText: /^$/ }).first()
+        await expandDetailBtn.click()
+    }
+    async clickCheckboxBySubject(subjectName:string){
+        const card = this.page.locator('.input-and-title').filter({hasText:subjectName})
+        const checkbox = card.locator('#appication_form')
+        await checkbox.click()
+    }
+
+    async clickExportButton(){
+        const exportBtn = this.page.getByRole('button', { name: 'EXPORT' })
+        await exportBtn.click()
+    }
+
+    async handleExportPopup(){
+        const heading = this.page.getByRole('heading', { name: 'คุณต้องการนำออกไฟล์ ?' })
+        const confirm = this.page.getByRole('button', { name: 'ยืนยัน' })
+        await expect(heading).toBeVisible()
+        await confirm.click()
+    }
 }
--- tests/Officer/nida_learning_eligible_list.spec.ts
+++ tests/Officer/nida_learning_eligible_list.spec.ts
@@ -31,4 +31,38 @@ test.describe('Test Script - NIDA Backoffice โมดูล ADM งานรั
         })
         await eligibleLearningPage.clickExpandDetailButtonByName('รอบที่ 7/2569')
     });
+
+    test('TC-04 ทดสอบนำออกรายชื่อผู้มีสิทธิ์สอบประจำโครงการ' , async ({ commonPage , eligibleLearningPage , page}) => {
+        await commonPage.gotoPrograms()
+        await eligibleLearningPage.gotoEligibleLearningMenu()
+        await expect(page).toHaveURL(/.*admin\/admission\/transaction\/eligible-candidates-list-for-program/);
+        await eligibleLearningPage.checkLearningEligibleMenu()
+        await eligibleLearningPage.clickAnyTabByKeyword('ประกาศรายชื่อผู้มีสิทธิ์สอบประจำโครงการ')
+        // await eligibleLearningPage.fillSearchBox('วิชาเฉพาะ 2')
+        await eligibleLearningPage.clickStatusByKeyword('ใช้งาน')
+        await eligibleLearningPage.filterMoreOption({
+            eduYear:'2569',
+            semester:'ภาคการศึกษาที่ 1',
+            eduLevel:'ปริญญาโท',
+            studentType:'ภาคปกติ',
+            round:'1',
+        })
+        await eligibleLearningPage.clickExpandDetailButtonByName('รอบที่ 1/2569 (ภาคการศึกษาที่ 1)')
+        await eligibleLearningPage.clickVerifyDetailButtonByCard('รอบที่ 1/2569 (ภาคการศึกษาที่ 1)')
+        await expect(page).toHaveURL(/.*admin\/admission\/transaction\/eligible-candidates-list-for-program\/detail.*/);
+        await eligibleLearningPage.checkEligibleStudentDetailPage('รอบที่ 1/2569')
+        await eligibleLearningPage.fillSearchEligibleLearningStudent('ทดสอบ')
+        const searchInput = {
+            status:undefined, 
+            faculty:'คณะบริหารธุรกิจ',
+            course:'บริหารธุรกิจมหาบัณฑิต',
+            program:'บริหารธุรกิจ ภาคปกติ 01'
+        }
+        await eligibleLearningPage.filterEligibleSearchOption(searchInput)
+        await eligibleLearningPage.clickExpandDetailButtonByCourse('บริหารธุรกิจ ภาคปกติ 01 ศูนย์การศึกษาจังหวัดกรุงเทพมหานคร ครั้งที่ 1 ปี 2569')
+        await eligibleLearningPage.clickCheckboxBySubject('บริหารธุรกิจ ภาคปกติ 01 ศูนย์การศึกษาจังหวัดกรุงเทพมหานคร ครั้งที่ 1 ปี 2569')
+        await eligibleLearningPage.clickExportButton()
+        await eligibleLearningPage.handleExportPopup()
+        await commonPage.clickBackToFirstPage()
+    });
 });
