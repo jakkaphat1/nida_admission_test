f3e2e15c825377f5c9942a691e9f46aae80849ca
jakkaphat1 <jakkaphat.w@kkumail.com>
2026-03-18 16:18:19 +0700

    [Add]-> TC-03 ทดสอบค้นหารายการหลักสูตรที่เปิดสมัครเรียน

--- pages/Student/EligibleLearningPage.ts
+++ pages/Student/EligibleLearningPage.ts
@@ -34,4 +34,68 @@ export class EligibleLearningPage {
         await expect(notAnnouce).toBeVisible();
         await expect(annouced).toBeVisible()
     }
+
+    //method ค้นหาในกล่องค้นหา
+    async fillSearchBox(searchKeyword:string){
+        const searchBox = this.page.getByRole('textbox', { name: 'ค้นหาจากรหัส หรือชื่อหลักสูตรและโครงการ' })
+        await searchBox.pressSequentially(searchKeyword)
+    }
+
+    async filterMoreOption(data:{
+        eduYear?:string
+        semester?:string
+        eduLevel?:string
+        studentType?:string
+        round?:string
+
+    }){
+        const filterBtn = this.page.getByRole('button', { name: 'ตัวกรอง' })
+        const backfilterBtn = this.page.getByRole('button', { name: 'ตัวกรอง' }).nth(1)
+        await filterBtn.click()
+        if(data.eduYear){
+            const eduYearDropdown = this.page.locator('div').filter({ hasText: /^เลือกปีการศึกษา$/ }).nth(3)
+            const eduYearOption = this.page.getByRole('option', { name: data.eduYear })
+            await eduYearDropdown.click()
+            await eduYearOption.click()
+        }
+        if(data.semester){
+            const semesterDropdown = this.page.locator('div').filter({ hasText: /^เลือกภาคการศึกษา$/ }).nth(3)
+            const semesterOption = this.page.getByRole('option', { name: data.semester })
+            await semesterDropdown.click()
+            await semesterOption.click()
+        }
+        if(data.eduLevel){
+            const eduLevelDropdown = this.page.locator('div').filter({ hasText: /^เลือกระดับการศึกษา$/ }).nth(3)
+            const eduLevelOption = this.page.getByRole('option', { name: data.eduLevel })
+            await eduLevelDropdown.click()
+            await eduLevelOption.click()
+        }
+        if(data.studentType){
+            const studentTypeDropdown = this.page.locator('div').filter({ hasText: /^เลือกประเภทนักศึกษา$/ }).nth(3)
+            const studentTypeOption = this.page.getByRole('option', { name: data.studentType })
+            await studentTypeDropdown.click()
+            await studentTypeOption.click()
+        }
+        if(data.round){
+            const roundDropdown = this.page.locator('div').filter({ hasText: /^เลือกรอบที่$/ }).nth(3)
+            const roundOption = this.page.getByRole('option', { name: data.round , exact:true } )
+            await roundDropdown.click()
+            await roundOption.click()
+        }
+        await backfilterBtn.click()
+    }
+
+    async clickExpandDetailButtonByName(cardName:string){
+        const card = this.page.locator('div').filter({ hasText: cardName }).nth(5)
+        const expandDetailBtn = card.getByRole('button').filter({ hasText: /^$/ }).first()
+        await expandDetailBtn.click()
+    }
+
+    async clickStatusByKeyword(status:string){
+        const statusDropdown = this.page.locator('.react-select__value-container').first()
+        const statusOption = this.page.getByRole('option', { name: status, exact: true })
+        await statusDropdown.click()
+        await statusOption.click()
+    }
+
 }
--- tests/Officer/nida_learning_eligible_list.spec.ts
+++ tests/Officer/nida_learning_eligible_list.spec.ts
@@ -13,4 +13,22 @@ test.describe('Test Script - NIDA Backoffice โมดูล ADM งานรั
         await expect(page).toHaveURL(/.*admin\/admission\/transaction\/eligible-candidates-list-for-program/);
         await eligibleLearningPage.checkLearningEligibleMenu()
     });
+
+    test('TC-03 ทดสอบค้นหารายการหลักสูตรที่เปิดสมัครเรียน' , async ({ commonPage , eligibleLearningPage , page}) => {
+        await commonPage.gotoPrograms()
+        await eligibleLearningPage.gotoEligibleLearningMenu()
+        await expect(page).toHaveURL(/.*admin\/admission\/transaction\/eligible-candidates-list-for-program/);
+        await eligibleLearningPage.checkLearningEligibleMenu()
+
+        // await eligibleLearningPage.fillSearchBox('วิชาเฉพาะ 2')
+        await eligibleLearningPage.clickStatusByKeyword('ใช้งาน')
+        await eligibleLearningPage.filterMoreOption({
+            eduYear:'2569',
+            semester:'ภาคการศึกษาที่ 1',
+            eduLevel:'ปริญญาโท',
+            studentType:'ภาคปกติ',
+            round:undefined,
+        })
+        await eligibleLearningPage.clickExpandDetailButtonByName('รอบที่ 7/2569')
+    });
 });
