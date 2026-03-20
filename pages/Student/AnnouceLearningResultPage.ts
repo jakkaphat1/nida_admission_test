3a7d8476b153c2c9dd6dad9f364f9ecfbcba4740
jakkaphat1 <jakkaphat.w@kkumail.com>
2026-03-20 15:28:52 +0700

    [Add]-> TC-07.2 ทดสอบกรณีดูไฟล์ประกาศ

--- pages/Student/AnnouceLearningResultPage.ts
+++ pages/Student/AnnouceLearningResultPage.ts
@@ -342,4 +342,10 @@ export class AnnouceLearningResultPage {
         await hoverPlace.hover()
         await clearBtn.click()
     }
+
+    async clickPDFButtonForViewAnnouceByNumber(number:string){
+        const card = this.page.locator('div').filter({ hasText: number}).nth(5)
+        const viewButton = this.page.getByRole('button', { name: 'เปิดดู PDF' })
+        await viewButton.click()
+    }
 }
--- tests/Officer/nida_annouce_learning_result.spec.ts
+++ tests/Officer/nida_annouce_learning_result.spec.ts
@@ -169,4 +169,23 @@ test.describe('Test Script - NIDA Backoffice โมดูล ADM งานรั
         await annouceLearningResultPage.clickConfirmReUploadAnnoucementPopup()
         await expect(page.getByText('แก้ไขประกาศสำเร็จ')).toBeVisible()
     });
+
+    test('TC-07.2 ทดสอบกรณีดูไฟล์ประกาศ' , async ({ commonPage , annouceLearningResultPage , page}) => {
+        await commonPage.gotoPrograms()
+        await annouceLearningResultPage.gotoAnnouceLearningResultMenu()
+        await expect(page).toHaveURL(/.*admin\/admission\/transaction\/selection-results-announcement.*/);
+        await annouceLearningResultPage.checkAnnouceLearningResultMenu()
+        await annouceLearningResultPage.clickAnyTabByKeyword('ประกาศรายชื่อผลการคัดเลือก')
+        // await annouceLearningResultPage.fillSearchBox('สถิติ')
+        await annouceLearningResultPage.filterMoreOption({
+            eduYear:'2568',
+            semester:'ภาคการศึกษาที่ 1',
+            round:'1',
+            // eduLevel:'ปริญญาโท',
+            studentType:'นานาชาติ' 
+        })
+        await annouceLearningResultPage.clickSeeAnnoucementButtonByCard('รอบที่ 1/2568 (ภาคการศึกษาที่ 1)')
+        await expect(page).toHaveURL(/.*admin\/admission\/transaction\/selection-results-announcement\/view-announcement.*/);
+        await annouceLearningResultPage.clickPDFButtonForViewAnnouceByNumber('20032026144302rHg')
+    });
 });
